92a4f68de2dc1a9ab9c440f2c34120293e1a2a5b
Jerzy Reclaw <jerzyreclaw@wp.pl>
2019-11-26 17:02:45 +0100

    labs done

---
+++ src/main/java/domain/Jumper.java
@@ -0,0 +1,117 @@
+package domain;
+
+import java.sql.Date;
+import java.util.Objects;
+
+public class Jumper {
+
+    private int id;
+    private String name;
+    private String surname;
+    private Date date_of_birth;
+    private Double personal_best;
+    private int carrer_wins;
+    private int team;
+
+    public Jumper() {
+    }
+
+
+    public Jumper(int id, String name, String surname, Date date_of_birth, double personal_best, int carrer_wins, int team) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.date_of_birth = date_of_birth;
+        this.personal_best = personal_best;
+        this.carrer_wins = carrer_wins;
+        this.team = team;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Date getDate_of_birth() {
+        return date_of_birth;
+    }
+
+    public void setDate_of_birth(Date date_of_birth) {
+        this.date_of_birth = date_of_birth;
+    }
+
+    public Double getPersonal_best() {
+        return personal_best;
+    }
+
+    public void setPersonal_best(double personal_best) {
+        this.personal_best = personal_best;
+    }
+
+    public int getCarrer_wins() {
+        return carrer_wins;
+    }
+
+    public void setCarrer_wins(int carrer_wins) {
+        this.carrer_wins = carrer_wins;
+    }
+
+    public int getTeam() {
+        return team;
+    }
+
+    public void setTeam(int team) {
+        this.team = team;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Jumper jumper = (Jumper) o;
+        return id == jumper.id &&
+                carrer_wins == jumper.carrer_wins &&
+                team == jumper.team &&
+                Objects.equals(name, jumper.name) &&
+                Objects.equals(surname, jumper.surname) &&
+                Objects.equals(date_of_birth, jumper.date_of_birth) &&
+                Objects.equals(personal_best, jumper.personal_best);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, surname, date_of_birth, personal_best, carrer_wins, team);
+    }
+
+    @Override
+    public String toString() {
+        return "Jumper{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", date_of_birth=" + date_of_birth +
+                ", personal_best=" + personal_best +
+                ", carrer_wins=" + carrer_wins +
+                ", team=" + team +
+                '}';
+    }
+}
---
+++ src/main/java/domain/Team.java
@@ -0,0 +1,79 @@
+package domain;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Team {
+    private int id;
+    private String name;
+    private String sponsor;
+    private Double budget;
+
+    public Team(){}
+
+    public Team(int id, String name, String sponsor, Double budget) {
+        this.id = id;
+        this.name = name;
+        this.sponsor = sponsor;
+        this.budget = budget;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSponsor() {
+        return sponsor;
+    }
+
+    public void setSponsor(String sponsor) {
+        this.sponsor = sponsor;
+    }
+
+    public Double getBudget() {
+        return budget;
+    }
+
+    public void setBudget(Double budget) {
+        this.budget = budget;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Team team = (Team) o;
+        return id == team.id &&
+                Double.compare(team.budget, budget) == 0 &&
+                name.equals(team.name) &&
+                sponsor.equals(team.sponsor);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, sponsor, budget);
+    }
+
+    @Override
+    public String toString() {
+        return "Team{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", sponsor='" + sponsor + '\'' +
+                ", budget=" + budget +
+                '}';
+    }
+}
